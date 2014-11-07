ab4373acd80bbad3fc5090483dc6f9c7183154f9
= <lord.thom@gmail.com>
2014-11-07 19:40:06 +0000

    Added pry to rspec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require './lib/regexp-examples.rb'
+require 'pry'
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
