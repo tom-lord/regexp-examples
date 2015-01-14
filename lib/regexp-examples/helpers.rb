28b707b0644ab45483c1d31d3177681f72037ade
Tom Lord <tom.lord@comlaude.com>
2015-01-14 11:11:37 +0000

    Removed old debugging helper

--- lib/regexp-examples/helpers.rb
+++ lib/regexp-examples/helpers.rb
@@ -32,18 +32,5 @@ module RegexpExamples
       CaptureGroupResult.new(nil, subgroups, result.join)
     end
   end
-
-  # TODO: For debugging only!! Delete this before v1.0
-  def self.show(regexp)
-    s = regexp.examples
-    puts "#{regexp.inspect} --> #{s.inspect}"
-    puts "Checking..."
-    errors = s.reject {|string| string =~ regexp}
-    if errors.size == 0
-      puts "All strings match"
-    else
-      puts "These don't match: #{errors.inspect}"
-    end
-  end
 end
 
