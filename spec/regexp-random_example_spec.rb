fe506bdf20b4bd2abb6a954fe4375fe370404462
Tom Lord <tom.lord@comlaude.com>
2015-03-08 12:58:52 +0000

    Some simple "smoke tests" for Regexp#random_example
    Possible future tests to add, if needed:
    * Performance test
    * "How random is it?" test

---
+++ spec/regexp-random_example_spec.rb
@@ -0,0 +1,22 @@
+RSpec.describe Regexp, "#random_example" do
+  def self.random_example_matches(*regexps)
+    regexps.each do |regexp|
+      it "random example for /#{regexp.source}/" do
+        random_example = regexp.random_example
+
+        expect(random_example).to be_a String # Not an Array!
+        expect(random_example).to match(Regexp.new("\\A(?:#{regexp.source})\\z", regexp.options))
+      end
+    end
+  end
+
+  context "smoke tests" do
+    # Just a few "smoke tests", to ensure the basic method isn't broken.
+    # Testing of the RegexpExamples::Parser class is all covered by Regexp#examples test already.
+    random_example_matches(
+      /\w{10}/,
+      /(we(need(to(go(deeper)?)?)?)?) \1/,
+      /case insensitive/i
+    )
+  end
+end
