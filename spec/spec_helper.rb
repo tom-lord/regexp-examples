b3d8bcc085c7591dc605182b76f0ead3c9a82cc5
Tom Lord <tom.lord@comlaude.com>
2015-12-04 17:37:58 +0000

    Suppress noisy warnings from tests
    Some tests include intentionally "bad" code, to thoroughly test the
    coverage of the gem.
    This was annoying me, so I've disabled it.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,13 @@ require './lib/regexp-examples.rb'
 require 'helpers'
 require 'pry'
 
+# Several of these tests (intentionally) use "weird" regex patterns,
+# that spam annoying warnings when running.
+# E.g. warning: invalid back reference: /\k/
+# and  warning: character class has ']' without escape: /[]]/ 
+# This config disables those warnings.
+$VERBOSE = nil
+
 RSpec.configure do |config|
   config.include Helpers
 
